9275d3817fa19ccdd82e053647d14b507ea24d1f
lby9906 <lby9906@naver.com>
2023-11-18 22:24:23 +0900

    bj25304 알고리즘

--- src/week01/Bj25304.java
+++ src/week01/Bj25304.java
@@ -1,2 +1,29 @@
-package week01;public class Bj25304 {
+package week01;
+
+import java.util.Scanner;
+
+public class Bj25304 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int sum = 0;
+        int num = 0;
+        int product;
+        int count;
+        int total = 0;
+
+        sum = in.nextInt();
+        num = in.nextInt();
+        for (int i = 0; i < num; i++) {
+            product = in.nextInt();
+            count = in.nextInt();
+            total += (product * count);
+        }
+        if (sum == total) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+    }
 }
