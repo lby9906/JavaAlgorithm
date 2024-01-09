4fe6fbdaa5dbcf4d899333754c88845b74ebeaec
lby9906 <lby9906@naver.com>
2024-01-09 23:04:11 +0900

    week05 코드리뷰 반영

--- src/week05/pg12928.java
+++ src/week05/pg12928.java
@@ -1,10 +1,6 @@
 package week05;
 
 public class pg12928 {
-
-    public pg12928() {
-    }
-
     public int solution(int n) {
         int answer = 0;
         for (int i=1; i<=n; i++) {
@@ -12,14 +8,10 @@ public class pg12928 {
                 answer += i;
             }
         }
-
         return answer;
     }
-
-
-
     public static void main(String[] args) {
         pg12928 dd = new pg12928();
-        dd.solution(5);
+        dd.solution(12);
     }
 }
--- src/week05/pg12933.java
+++ src/week05/pg12933.java
@@ -3,14 +3,8 @@ package week05;
 import java.util.Arrays;
 
 public class pg12933 {
-
-    public pg12933() {
-    }
-
     public long solution(long n) {
-        long answer = 0;
-        long num = n;
-        String str = String.valueOf(num);
+        String str = String.valueOf(n);
         String[] arr = str.split("");
 
         Arrays.sort(arr);
@@ -19,16 +13,12 @@ public class pg12933 {
         for (int i= arr.length-1; i>=0; i--) {
             sortStr += arr[i];
         }
-
-        answer = Long.parseLong(sortStr);
-
-        return answer;
+        return Long.parseLong(sortStr);
     }
 
     public static void main(String[] args) {
         pg12933 dd = new pg12933();
         dd.solution(118372);
-
     }
 
 }
